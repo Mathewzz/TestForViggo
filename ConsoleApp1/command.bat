4d6cee6b5c034a76fc0cbf922ef64b43a6bcddfb
Stig Buhl <stig@komin.dk>
2020-02-13 09:53:02 +0100

    Made wierd changes

--- ConsoleApp1/command.bat
+++ ConsoleApp1/command.bat
@@ -1,6 +1,6 @@
 for /f "delims=" %%i in ('git symbolic-ref --short HEAD') do set output=%%i
 git checkout master
 git pull origin master
-git merge output
+git merge %output%
 git push origin master
 PAUSE
